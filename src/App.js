0b0e2d42590f198156e75da0a2b26e7aea9bc404
坂本元気 <GENKI.S@sakamotogenki-no-MacBook-Air.local>
2018-08-04 08:07:17 +0900

    jsx

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello world!!</h1>
-      </div>
-    );
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => {console.log("Im clicked!")}} />
+      </React.Fragment>
+    )
   }
 }
 
