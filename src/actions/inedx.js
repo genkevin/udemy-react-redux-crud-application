0844481419d674f90bf0e50569fac511fc238bc2
坂本元気 <GENKI.S@sakamotogenki-no-MacBook-Air.local>
2018-08-04 12:57:03 +0900

    redux

---
+++ src/actions/inedx.js
@@ -0,0 +1,10 @@
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+
+export cconst increment = () => ({
+  type: 'INCREMENT'
+})
+
+export cconst decrement = () => ({
+  type: 'DECREMENT'
+})
